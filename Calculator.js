2b9e49491c806f059f194a19588be6893ff9545d
SwedishArtist <32638426+SwedishArtist@users.noreply.github.com>
2018-02-05 13:33:35 +0100

    påbörjat addition och reset

--- Calculator.js
+++ Calculator.js
@@ -9,7 +9,10 @@ let btn = document.createElement("button");
     calculator.appendChild(btn);
 }
 let stack = [];
+stack.length = 9;
 const output = document.getElementsByTagName("output")[0];
+
+let operand = [];
 
 calculator.addEventListener ("click", (e)=> {
 	let btn = e.target;
@@ -17,27 +20,32 @@ calculator.addEventListener ("click", (e)=> {
 	if (btn.className === "number") {
 		stack.push(btn.innerHTML);
 		// console.log(stack);
+	} else if (btn ===  document.getElementById('plus')) {
+		operand.push(btn.innerHTML);
+		console.log(operand);
 	}
 	doOutput();
 });
 const doOutput = () => {
 	let text = stack.join("");
-	output.value = text;
+	let disText = text.substring(0, 9);
+	output.value = disText;
 };
 
 
+/* Ex. 3,2,1 behöver bli 321
 
+operand, operator
 
+if (plustecken) operands.push
+o = +stack.join("")      + är bara för att ändra strängen till ett tal +"32" = 32.
+operands.push
 
+Array.join(arr)   join gör det till en string
 
-
-
-function buttons() {
-
-}
-document.getElementById("calculator").addEventListener("click", buttons, false);
-
-
+Om plustecken nedtryckt
+Konvertera strecken till sträng (görs med: join) och sedan till number (görs med: +)
+lägg "stacken" i array operands. */
 
 
 
